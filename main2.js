f06d18d7f38005497d500a77137e81f795cd8ea1
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-19 18:44:08 +0100

    eseguito selfwork condizioni2

--- main2.js
+++ main2.js
@@ -1,2 +1,42 @@
-let a = `ciao`
-console.log(a);
+let temp = prompt(`Inserisci il valore della temperatura`)
+
+if (temp < -10) {
+    console.log(`Copriti...ancora ti raffreddi`);
+} 
+else if (temp >= -10 && temp < 0) {
+    console.log(`Non è tanto il freddo quanto l'umidità`);
+}
+else if (temp >= 0 && temp < 20){
+    console.log(`Non ci sono più le mezze stagioni`);
+}
+else if (temp >= 20 && temp < 30){
+    console.log(`Mi dia una peroni sudata`);
+}
+else if (temp >= 30){
+    console.log(`Lu mare, lu sole, lu ientu`);
+}else{
+    console.log(`Valore non valido`);
+}
+
+
+
+switch (true) {
+    case temp < -10:
+    console.log(`Copriti...ancora ti raffreddi`);
+    break;
+    case temp >= -10 && temp < 0:
+    console.log(`Non è tanto il freddo quanto l'umidità`);
+    break;    
+    case temp >= 0 && temp < 20:
+    console.log(`Non ci sono più le mezze stagioni`);
+    break;   
+    case temp >= 20 && temp < 30:
+    console.log(`Mi dia una peroni sudata`);
+    break;   
+    case temp >= 30:
+    console.log(`Lu mare, lu sole, lu ientu`);
+    break;
+    default:
+    console.log(`Valore non valido`);
+    break;
+}
